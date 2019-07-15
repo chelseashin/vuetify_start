eed0fdb17ff540ad8fa247718b2c9ab8f16ad1df
ChaewonShin <chaewonshin95@gmail.com>
2019-07-15 11:42:48 +0900

    190715 | vuetify start

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-toolbar app dark height="150" class="purple" >
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Hi</span>
+        <span class="font-weight-light">Five</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
