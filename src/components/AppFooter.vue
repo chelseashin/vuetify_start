d7fab1bc8652655a0f3197b7e47473b146ddb5bd
chelseashin <chaewonshin95@gmail.com>
2019-08-04 23:43:31 +0900

    190804 | hifive_footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -20,6 +20,7 @@
       </v-card-text>
 
       <v-card-text class="white--text pt-0">
+        Copyright ⓒ HiFive Inc. All rights reserved<br>
         Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
       </v-card-text>
 
@@ -37,10 +38,8 @@ export default {
     data: () => ({
       icons: [
         'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
         'fab fa-instagram',
+        'fab fa-google-plus'
       ],
     }),
   }
